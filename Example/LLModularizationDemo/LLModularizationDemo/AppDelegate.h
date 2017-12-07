daf55f753356209f0adf6f2abcc025771b49e129
李林 <hzlilin3@corp.netease.com>
2017-12-07 20:13:44 +0800

    first commit

---
+++ Example/LLModularizationDemo/LLModularizationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LLModularizationDemo
+//
+//  Created by 李林 on 2017/12/7.
+//  Copyright © 2017年 lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/LLModularizationDemo/LLModularizationDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LLModularizationDemo
+//
+//  Created by 李林 on 2017/12/7.
+//  Copyright © 2017年 lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
