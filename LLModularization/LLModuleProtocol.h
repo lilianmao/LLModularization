f8b7c3fe4ea2d8def7cb70d01a9660ea81df9431
李林 <hzlilin3@corp.netease.com>
2017-12-07 20:49:51 +0800

    基本完成工程一些初始化工作

---
+++ LLModularization/LLModuleProtocol.h
@@ -0,0 +1,12 @@
+//
+//  LLModuleProtocol.h
+//  Pods-LLModularizationDemo
+//
+//  Created by 李林 on 2017/12/7.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol LLModuleProtocol <NSObject>
+
+@end
