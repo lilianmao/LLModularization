36c16fcb942d5d6581c9939ebd9811b415cc9367
lilin <1911932429@qq.com>
2018-01-26 14:41:30 +0800

    暂存一个版本

--- showLLModularization/gulpfile.js
+++ showLLModularization/gulpfile.js
@@ -9,3 +9,8 @@ gulp.task('moveBootstrap' ,['moveJquery'] ,  function(){
     return gulp.src('node_modules/bootstrap/*/*/*')
         .pipe(gulp.dest('public/libs/bootstrap/'));
 });
+
+gulp.task('moveBootstrap-table', ['moveBootstrap'], function () {
+   return gulp.src('node_modules/bootstrap-table/*/*/*')
+       .pipe(gulp.dest('public/libs/bootstrap-table'))
+});
