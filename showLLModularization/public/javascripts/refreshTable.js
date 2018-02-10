acf2b03aab111aee5510c2dfe4c1996fe2060bb6
lilin <1911932429@qq.com>
2018-02-10 20:36:19 +0800

    修改前端页面

--- showLLModularization/public/javascripts/refreshTable.js
+++ showLLModularization/public/javascripts/refreshTable.js
@@ -20,9 +20,43 @@ function refreshTable(data) {
         submitTypeElement.append(data[item].submitType);
         trElement.append(submitTypeElement);
         let dateElement = document.createElement('td');
-        dateElement.append(data[item].date);
+        var date = convertDateFromString(data[item].date);
+        var dateStr = date.format("yyyy-MM-dd hh:mm:ss");
+        dateElement.append(dateStr);
         trElement.append(dateElement);
 
         $("#myTable").append(trElement);
+    }
+}
+
+Date.prototype.format = function(fmt) {
+    var o = {
+        "M+" : this.getMonth()+1,                 //月份
+        "d+" : this.getDate(),                    //日
+        "h+" : this.getHours(),                   //小时
+        "m+" : this.getMinutes(),                 //分
+        "s+" : this.getSeconds(),                 //秒
+        "q+" : Math.floor((this.getMonth()+3)/3), //季度
+        "S"  : this.getMilliseconds()             //毫秒
+    };
+    if(/(y+)/.test(fmt)) {
+        fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+    }
+    for(var k in o) {
+        if(new RegExp("("+ k +")").test(fmt)){
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+        }
+    }
+    return fmt;
+}
+
+function convertDateFromString(dateString) {
+    if (dateString) {
+        console.log(dateString);
+        var arr1 = dateString.split("T");
+        var sdate = arr1[0].split('-');
+        var tdate = arr1[1].split('.')[0].split(':');
+        var date = new Date(sdate[0], sdate[1]-1, sdate[2], tdate[0], tdate[1], tdate[2]);
+        return date;
     }
 }
